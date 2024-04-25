a0838bff5fbdd2a6654cdb15382166aaf6e9dc0f
Declan Middleton <declanredd1722@gmail.com>
2024-04-25 20:42:29 +1200

    Add files via upload

---
+++ hak-tools.sh
@@ -0,0 +1,17 @@
+brew install git 
+brew install --cask burp-suite
+brew install wireshark
+brew install nmap
+brew install gobuster
+brew install nikto
+brew install wpscanteam/tap/wpscan
+brew install --cask metasploit
+brew install exploitdb
+brew install hashcat
+brew install sambabrew install binwalk
+brew install exiftool
+brew install aircrack-ng
+brew install hydra
+brew install sqlmap
+echo "Done installing! Start hacking away.."
+
