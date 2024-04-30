630df0d90bc1f54ee74e0aa20cfde1e3587233e7
Declan Middleton <declanredd1722@gmail.com>
2024-05-01 11:57:24 +1200

    Update hak-tools.sh

--- hak-tools.sh
+++ hak-tools.sh
@@ -12,7 +12,22 @@ brew install exploitdb
 brew install hashcat
 brew install sambabrew install binwalk
 brew install exiftool
+brew install aircrack-ng
+brew install netcat
+brew install arp-scan
+brew install openvpn
+brew install php
+brew install tcpdump
+brew install exploitdb
+brew install openssh
 brew install hydra
+brew install ffuf
+brew install theharvester
+brew install nuclei
+brew install ettercap
+brew install subfinder
+brew install httrack
+brew install mysql
 brew install sqlmap
 echo "Done installing! Start hacking away.."
 
