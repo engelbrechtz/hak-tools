b493195ead255d410ca765c2653770461197cefe
Declan Middleton <declanredd1722@gmail.com>
2024-05-01 12:35:26 +1200

    Create hak-basic-install.sh

---
+++ hak-basic-install.sh
@@ -0,0 +1,20 @@
+echo "Started Installation.."
+brew install git 
+brew install --cask burp-suite
+brew install nmap
+brew install gobuster
+brew install nikto
+brew install --cask metasploit
+brew install exploitdb
+brew install hashcat
+brew install netcat
+brew install openvpn
+brew install php
+brew install exploitdb
+brew install openssh
+brew install ffuf
+brew install subfinder
+brew install httrack
+brew install mysql
+brew install sqlmap
+echo "Done installing! Start hacking away.."
